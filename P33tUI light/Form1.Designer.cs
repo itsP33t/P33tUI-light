7fb42d3602726468fd66e66d5bc4a1a7582ae95a
Peter <79012293+itsP33t@users.noreply.github.com>
2021-02-21 22:55:06 +0100

    LightV1
    free c# UI for your projects. Say stop to ugly projects
    
    With this template you can create beautiful projects in matter of seconds. Works for C# Windows Forms
    
    If you like what im doing and want to support me (free or paid) go to https://www.p33t.net/supportme.html
    
    Happy Coding :)

---
+++ P33tUI light/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace P33tUI_light
+{
+    partial class P33tUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(P33tUI));
+            this.siticoneControlBox1 = new Siticone.UI.WinForms.SiticoneControlBox();
+            this.siticoneControlBox2 = new Siticone.UI.WinForms.SiticoneControlBox();
+            this.siticoneDragControl1 = new Siticone.UI.WinForms.SiticoneDragControl(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.siticoneButton1 = new Siticone.UI.WinForms.SiticoneButton();
+            this.SuspendLayout();
+            // 
+            // siticoneControlBox1
+            // 
+            this.siticoneControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.siticoneControlBox1.FillColor = System.Drawing.Color.White;
+            this.siticoneControlBox1.HoveredState.Parent = this.siticoneControlBox1;
+            this.siticoneControlBox1.IconColor = System.Drawing.Color.Black;
+            this.siticoneControlBox1.Location = new System.Drawing.Point(901, 12);
+            this.siticoneControlBox1.Name = "siticoneControlBox1";
+            this.siticoneControlBox1.ShadowDecoration.Parent = this.siticoneControlBox1;
+            this.siticoneControlBox1.Size = new System.Drawing.Size(45, 29);
+            this.siticoneControlBox1.TabIndex = 0;
+            // 
+            // siticoneControlBox2
+            // 
+            this.siticoneControlBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.siticoneControlBox2.ControlBoxType = Siticone.UI.WinForms.Enums.ControlBoxType.MinimizeBox;
+            this.siticoneControlBox2.FillColor = System.Drawing.Color.White;
+            this.siticoneControlBox2.HoveredState.Parent = this.siticoneControlBox2;
+            this.siticoneControlBox2.IconColor = System.Drawing.Color.Black;
+            this.siticoneControlBox2.Location = new System.Drawing.Point(850, 12);
+            this.siticoneControlBox2.Name = "siticoneControlBox2";
+            this.siticoneControlBox2.ShadowDecoration.Parent = this.siticoneControlBox2;
+            this.siticoneControlBox2.Size = new System.Drawing.Size(45, 29);
+            this.siticoneControlBox2.TabIndex = 1;
+            // 
+            // siticoneDragControl1
+            // 
+            this.siticoneDragControl1.TargetControl = this;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lemon/Milk", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 194);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(936, 228);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Hello👋\r\nThanks for downloading my Form template.\r\nfeel free to use it on your pr" +
+    "ojects.\r\n\r\nif you like what i do and want to support me\r\ngo to https://www.p33t." +
+    "net/supportme.html";
+            // 
+            // siticoneButton1
+            // 
+            this.siticoneButton1.CheckedState.Parent = this.siticoneButton1;
+            this.siticoneButton1.CustomImages.Parent = this.siticoneButton1;
+            this.siticoneButton1.FillColor = System.Drawing.Color.Black;
+            this.siticoneButton1.Font = new System.Drawing.Font("Lemon/Milk", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.siticoneButton1.ForeColor = System.Drawing.Color.White;
+            this.siticoneButton1.HoveredState.Parent = this.siticoneButton1;
+            this.siticoneButton1.Location = new System.Drawing.Point(257, 449);
+            this.siticoneButton1.Name = "siticoneButton1";
+            this.siticoneButton1.ShadowDecoration.Parent = this.siticoneButton1;
+            this.siticoneButton1.Size = new System.Drawing.Size(445, 89);
+            this.siticoneButton1.TabIndex = 3;
+            this.siticoneButton1.Text = "open p33t.net";
+            this.siticoneButton1.Click += new System.EventHandler(this.siticoneButton1_Click);
+            // 
+            // P33tUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(958, 562);
+            this.Controls.Add(this.siticoneButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.siticoneControlBox2);
+            this.Controls.Add(this.siticoneControlBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "P33tUI";
+            this.Text = "P33tUI light";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Siticone.UI.WinForms.SiticoneControlBox siticoneControlBox1;
+        private Siticone.UI.WinForms.SiticoneControlBox siticoneControlBox2;
+        private Siticone.UI.WinForms.SiticoneDragControl siticoneDragControl1;
+        private Siticone.UI.WinForms.SiticoneButton siticoneButton1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ P33tUI light/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P33tUI_light
+{
+    public partial class P33tUI : Form
+    {
+        public P33tUI()
+        {
+            InitializeComponent();
+        }
+
+        private void siticoneButton1_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.p33t.net");
+        }
+    }
+}
